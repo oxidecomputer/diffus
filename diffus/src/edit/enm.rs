b3dbf18cd02a07896c5df7cff497f6e588e2e134
Andrew J. Stone <andrew@oxidecomputer.com>
2025-01-07 16:39:55 -0500

    Provide before and after data for enum associated changes (#7)
    Providing before and after contexts, allows users to stop the recursion
    at a depth of their choosing by getting back the original values.

--- diffus/src/edit/enm.rs
+++ diffus/src/edit/enm.rs
@@ -1,20 +1,15 @@
 #[cfg_attr(feature = "serialize-impl", derive(serde::Serialize))]
 #[derive(Debug, Eq, PartialEq)]
 pub enum Edit<'a, T: ?Sized, Diff> {
-    Copy(&'a T),
     VariantChanged(&'a T, &'a T),
-    AssociatedChanged(Diff),
+    AssociatedChanged {
+        before: &'a T,
+        after: &'a T,
+        diff: Diff,
+    },
 }
 
 impl<'a, T: ?Sized, Diff> Edit<'a, T, Diff> {
-    pub fn is_copy(&self) -> bool {
-        if let Self::Copy(_) = self {
-            true
-        } else {
-            false
-        }
-    }
-
     pub fn is_variant_changed(&self) -> bool {
         if let Self::VariantChanged(_, _) = self {
             true
@@ -24,7 +19,7 @@ impl<'a, T: ?Sized, Diff> Edit<'a, T, Diff> {
     }
 
     pub fn is_associated_changed(&self) -> bool {
-        if let Self::AssociatedChanged(_) = self {
+        if let Self::AssociatedChanged { .. } = self {
             true
         } else {
             false
@@ -40,8 +35,8 @@ impl<'a, T: ?Sized, Diff> Edit<'a, T, Diff> {
     }
 
     pub fn associated_change(&self) -> Option<&Diff> {
-        if let Self::AssociatedChanged(value) = self {
-            Some(value)
+        if let Self::AssociatedChanged { diff, .. } = self {
+            Some(diff)
         } else {
             None
         }
